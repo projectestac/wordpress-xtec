4a0f405b6595cf4aef0f76044e6f225c330ac06a
xaviernietosanchez <xavier.nieto.sanchez@upcnet.es>
2017-05-18 13:11:00 +0200

    Add a cron to remove old bookings

---
+++ scripts/script_xtec_booking.class.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('agora_script_base.class.php');
+
+class script_xtec_booking extends agora_script_base {
+
+    public $title = "Programa l'esborrament de les reserves antigues";
+    public $info = "Afegeix un cron al WordPress que s'executa diàriament i esborra les reserves que porten més d'un any caducades o modifica la data d'inici d'aquelles que van començar fa més d'un any";
+
+    protected function _execute($params = array()) {
+
+    	if (! wp_next_scheduled ( 'cron_xtec_booking' )) {
+    		wp_schedule_event ( time(), 'daily', 'cron_xtec_booking' );
+		}
+
+        return true;
+    }
+
+}
