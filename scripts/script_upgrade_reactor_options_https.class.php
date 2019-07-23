e3c0d73dc23e0e7c2fcf3234567da7af552e7e78
Toni Ginard <toni.ginard@gmail.com>
2019-07-23 14:15:05 +0200

    Added action to replace http by https in reactor_options

---
+++ scripts/script_upgrade_reactor_options_https.class.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('agora_script_base.class.php');
+
+class script_upgrade_reactor_options_https extends agora_script_base {
+
+    public $title = 'Corregeix enllaços http a https a reactor_options';
+    public $info = 'Edita el registre reactor_options i canvia els enllaços que comencen per http a https';
+
+    public function params() {
+        return [];
+    }
+
+    protected function _execute($params = []) {
+
+        $options = (get_option('reactor_options')) ? get_option('reactor_options') : null;
+        foreach ($options as $key => $value) {
+            if (($key == 'logo_image') || ($key == 'contacteCentre')) {
+                if (substr($value, 0, 7) === "http://") {
+                    $value         = preg_replace("/^http:/i", "https:", $value);
+                    $options[$key] = $value;
+                }
+            }
+        }
+
+        update_option('reactor_options', $options);
+
+        $this->output('Reactor options https upgrade completed');
+
+        return true;
+    }
+
+}
