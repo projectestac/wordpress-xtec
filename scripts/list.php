40b296c563f210f038ac43d8fa79c77c8cb94869
Toni Ginard <toni.ginard@gmail.com>
2024-07-09 17:59:06 +0200

    list.php: Improved code

--- scripts/list.php
+++ scripts/list.php
@@ -1,17 +1,23 @@
 <?php
 
-require( dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-load.php' );
-require_once(dirname(__FILE__).'/scripts.lib.php');
+require_once dirname(__FILE__, 4) . '/wp-load.php';
+require_once __DIR__ . '/scripts.lib.php';
 
 $scripts = get_all_scripts();
-$actions = array();
+$actions = [];
+
 foreach ($scripts as $scriptname => $script) {
+
     $action = new StdClass();
+
     $action->action = $scriptname;
     $action->title = $script->title;
     $action->description = $script->info;
     $scriptclass = new $scriptname();
-	$action->params = array_keys($scriptclass->params());
+    $action->params = array_keys($scriptclass->params());
+
     $actions[] = $action;
+
 }
+
 echo json_encode($actions);
