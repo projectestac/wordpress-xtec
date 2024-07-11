a39d39877fca83e617bafd8290aca5aa3cce2251
Toni Ginard <toni.ginard@gmail.com>
2024-07-11 14:27:18 +0200

    cli.php: Improved code

--- scripts/cli.php
+++ scripts/cli.php
@@ -1,26 +1,30 @@
 <?php
-define('CLI_SCRIPT', true);
-require( dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-load.php' );
-require_once(dirname(__FILE__).'/scripts.lib.php');
+
+const CLI_SCRIPT = true;
+
+require_once dirname(__FILE__, 4) . '/wp-load.php';
+require_once __DIR__ . '/scripts.lib.php';
+
 $script = get_cli_arg('s');
 
-echo "Start server Time: ".date('r', time())."\n";
+echo 'Start server Time: ' . date('r') . "\n";
 
 set_time_limit(0);
+
 try {
     $success = scripts_execute_script($script);
-} catch (Exception $e){
+} catch (Exception $e) {
     $success = false;
-    echo($e->getMessage());
+    echo ($e->getMessage());
 }
 
-echo "End server Time: ".date('r', time())."\n";
+echo 'End server Time: ' . date('r') . "\n";
 
 if ($success) {
-	echo 'Script '.$script.' succeed'."\n";
+    echo 'Script ' . $script . ' succeed' . "\n";
     echo 'success';
-	exit (0);
+    exit (0);
 } else {
-	echo 'Script '.$script.' failed'."\n";
-	exit ('error');
+    echo 'Script ' . $script . ' failed' . "\n";
+    exit ('error');
 }
